4f6f3c7fe2adf03ff6964e84484543ed120a7ebc
sakitamako <sa.919.1025.ki@gmail.com>
2024-05-09 21:22:42 +0900

    エラー解消、コードの入力ミス

--- sql/testdb.sql
+++ sql/testdb.sql
@@ -1,4 +1,4 @@
-drop databases if exists testdb;
+drop database if exists testdb;
 create database testdb;
 use testdb;
 create table test_table(
@@ -7,5 +7,5 @@ user_name varchar(255),
 password varchar(255));
 
 insert into test_table values(1, "taro", "123");
-insert into test_table values(2, "jiro", "456";)
+insert into test_table values(2, "jiro", "456");
 insert into test_table values(3, "hanako", "789");
